2bd3b4427c5020b6d88dcebc4b8dd2772a056020
NicoBarti <nicolas.barticevic@gmail.com>
2020-02-23 11:52:38 -0300

    agrego flikity, pero funciona con bad elemtn null

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,16 @@ document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
 })
+
+import Flickity from 'flickity'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elem = document.querySelector('.main-carousel');
+  const flkty = new Flickity(elem, {
+    fade: true,
+  });
+})
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
