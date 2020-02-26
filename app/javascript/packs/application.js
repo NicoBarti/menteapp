3e62bcf25b3ea023a5f2b81575e7860a110cd33f
NicoBarti <nicolas.barticevic@gmail.com>
2020-02-25 21:35:38 -0300

    añado imagenes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,12 +18,7 @@ document.addEventListener("turbolinks:load", () => {
 
 import Flickity from 'flickity'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const elem = document.querySelector('.main-carousel');
-  const flkty = new Flickity(elem, {
-    fade: true,
-  });
-})
+
 
 import "jquery-parallax.js"
 
@@ -54,16 +49,14 @@ require("../js/testimonials.js")
 require("../js/year.js")
 
 
-
-
-
-
-
+// document.addEventListener('DOMContentLoaded', () => {
+//
+// })
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
