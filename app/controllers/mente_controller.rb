e7ebd206132cae93a4ac566f8a3df3e390ae67d7
NicoBarti <nicolas.barticevic@gmail.com>
2020-05-03 02:45:54 -0400

    cambio vuelta a abouts con orden

--- app/controllers/mente_controller.rb
+++ app/controllers/mente_controller.rb
@@ -1,4 +1,6 @@
 class MenteController < ApplicationController
+  # before_action :set_about
+
 
   def index
     # @texto = 'text-muted'
@@ -6,7 +8,25 @@ class MenteController < ApplicationController
     # render 'mente/index.html.erb'
   end
 
+  def show
+    todos = About.all
+    about = About.where(:id => params[:id])
+    @abouts = about + (todos - about)
+    # bo = an
+
+    render 'mente/index'
+  end
+
   def abouts
     render 'abouts/index.html.erb'
   end
+
+  # private
+  #   # Use callbacks to share common setup or constraints between actions.
+  #   def set_about
+  #     params[:id] ? @about = About.find(params[:id]) : nil
+  #
+  #     # @about = About.find(params[:id])
+  #   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'acrcade/index'
   resources :blogs
   get 'mente/index'
+  get 'mente/:id', to: 'mente#show', as: :mente
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'mente#index'
 
