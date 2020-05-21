93a4da088298a801e1943a6814be852d522566b9
NicoBarti <nicolas.barticevic@gmail.com>
2020-05-20 21:02:39 -0400

    añade texto listo

--- db/migrate/20200521003019_agrega_texto_blog.rb
+++ db/migrate/20200521003019_agrega_texto_blog.rb
@@ -1,12 +1,12 @@
 class AgregaTextoBlog < ActiveRecord::Migration[6.0]
   def change
-      add_column :blogs, :texto3, :numeric
-      add_column :blogs, :texto4, :numeric
-      add_column :blogs, :texto5, :numeric
-      add_column :blogs, :texto6, :numeric
-      add_column :blogs, :texto7, :numeric
-      add_column :blogs, :texto8, :numeric
-      add_column :blogs, :texto9, :numeric
-      add_column :blogs, :texto10, :numeric
+      add_column :blogs, :texto3, :text
+      add_column :blogs, :texto4, :text
+      add_column :blogs, :texto5, :text
+      add_column :blogs, :texto6, :text
+      add_column :blogs, :texto7, :text
+      add_column :blogs, :texto8, :text
+      add_column :blogs, :texto9, :text
+      add_column :blogs, :texto10, :text
   end
 end
